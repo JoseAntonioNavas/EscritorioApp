a674c508d756631e65c7dde7c9152049fadde44b
JoseAntonioNavas <joseantonionavasgarcia1@gmail.com>
2021-01-18 20:31:26 +0100

    18Enero20:31

--- src/logic/globalVariables.java
+++ src/logic/globalVariables.java
@@ -6,6 +6,6 @@ public class globalVariables {
 
 	
 	public static Usuario usuarioLogged;
-	public final static String FILE_RECORDAR_USUARIO="archivos\\RecordarUsuario.dat";
+	public final static String FILE_RECORDAR_USUARIO="archivos\\RecordarUsuario.data";
 	
 }
